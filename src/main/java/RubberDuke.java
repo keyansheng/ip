9ed4698992395d4c1b767a28723895df0a1d445c
keyansheng <65121402+keyansheng@users.noreply.github.com>
2025-01-23 22:07:07 +0800

    Add prompt and echo

--- src/main/java/RubberDuke.java
+++ src/main/java/RubberDuke.java
@@ -1,17 +1,28 @@
+import java.util.Scanner;
+
 public class RubberDuke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
         greet();
+        while (!(input = prompt(scanner)).equals("bye")) {
+            System.out.println(input);
+        }
         farewell();
     }
 
     private static void greet() {
         System.out.println("""
                 Quack! I'm Rubber Duke, your friendly neighbourhood rubber duck, here to help you with your debugging sessions.
-                What can I do for you?
-                """);
+                What can I do for you?""");
     }
 
     private static void farewell() {
         System.out.println("Quack. Hope to see you again soon!");
+    }
+
+    private static String prompt(Scanner scanner) {
+        System.out.print("> ");
+        return scanner.nextLine();
     }
 }
