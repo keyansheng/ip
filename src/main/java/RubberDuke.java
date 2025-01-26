7b988579d15957187d403c85406d240a9b18bd26
keyansheng <65121402+keyansheng@users.noreply.github.com>
2025-01-26 12:17:53 +0800

    Change commands to return strings instead of printing

--- src/main/java/RubberDuke.java
+++ src/main/java/RubberDuke.java
@@ -10,19 +10,19 @@ public class RubberDuke {
         greet();
         while (!(input = prompt(scanner)).equals("bye")) {
             if (input.equals("list")) {
-                list(tasks);
+                System.out.println(list(tasks));
             } else if (input.startsWith("mark ")) {
-                mark(tasks, input.substring("mark ".length()));
+                System.out.println(mark(tasks, input.substring("mark ".length())));
             } else if (input.startsWith("unmark ")) {
-                unmark(tasks, input.substring("unmark ".length()));
+                System.out.println(unmark(tasks, input.substring("unmark ".length())));
             } else if (input.startsWith("delete ")) {
-                delete(tasks, input.substring("delete ".length()));
+                System.out.println(delete(tasks, input.substring("delete ".length())));
             } else if (input.startsWith("todo ")) {
-                addTodo(input.substring("todo ".length()), tasks);
+                System.out.println(addTodo(input.substring("todo ".length()), tasks));
             } else if (input.startsWith("deadline ")) {
-                addDeadline(input.substring("deadline ".length()), tasks);
+                System.out.println(addDeadline(input.substring("deadline ".length()), tasks));
             } else if (input.startsWith("event ")) {
-                addEvent(input.substring("event ".length()), tasks);
+                System.out.println(addEvent(input.substring("event ".length()), tasks));
             } else {
                 try {
                     throw new UnknownCommandException();
@@ -34,94 +34,114 @@ public class RubberDuke {
         farewell();
     }
 
-    private static void mark(List<Task> tasks, String number) {
+    private static String mark(List<Task> tasks, String number) {
+        StringBuilder output = new StringBuilder();
         try {
             Task task = tasks.get(Integer.parseInt(number.strip()) - 1);
             task.mark();
-            System.out.printf("Quack! I've marked this task as done:\n%s%n", task);
+            output.append("Quack! I've marked this task as done:\n").append(task);
         } catch (NumberFormatException e) {
-            System.out.println("Oh quack! I can't read this number! Please specify the task number.");
+            output.append("Oh quack! I can't read this number! Please specify the task number.");
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("Oh quack! I can't find this task! Please check the task number.");
+            output.append("Oh quack! I can't find this task! Please check the task number.");
         }
+        return output.toString();
     }
 
-    private static void unmark(List<Task> tasks, String number) {
+    private static String unmark(List<Task> tasks, String number) {
+        StringBuilder output = new StringBuilder();
         try {
             Task task = tasks.get(Integer.parseInt(number.strip()) - 1);
             task.unmark();
-            System.out.printf("Quack, I've marked this task as not done yet:\n%s%n", task);
+            output.append("Quack, I've marked this task as not done yet:\n").append(task);
         } catch (NumberFormatException e) {
-            System.out.println("Oh quack! I can't read this number! Please specify the task number.");
+            output.append("Oh quack! I can't read this number! Please specify the task number.");
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("Oh quack! I can't find this task! Please check the task number.");
+            output.append("Oh quack! I can't find this task! Please check the task number.");
         }
+        return output.toString();
     }
 
-    private static void delete(List<Task> tasks, String number) {
+    private static String delete(List<Task> tasks, String number) {
+        StringBuilder output = new StringBuilder();
         try {
             Task task = tasks.remove(Integer.parseInt(number.strip()) - 1);
-            System.out.printf("""
-                    Quack. I've removed this task:
-                    %s
-                    Now you have %d task%s in the list.
-                    """, task, tasks.size(), tasks.size() == 1 ? "" : "s");
+            output
+                    .append("Quack. I've removed this task:\n")
+                    .append(task)
+                    .append("\nNow you have ")
+                    .append(tasks.size())
+                    .append(" task")
+                    .append(tasks.size() == 1 ? "" : "s")
+                    .append(" in the list.");
         } catch (NumberFormatException e) {
-            System.out.println("Oh quack! I can't read this number! Please specify the task number.");
+            output.append("Oh quack! I can't read this number! Please specify the task number.");
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("Oh quack! I can't find this task! Please check the task number.");
+            output.append("Oh quack! I can't find this task! Please check the task number.");
         }
+        return output.toString();
     }
 
-    private static void addTodo(String description, List<Task> tasks) {
+    private static String addTodo(String description, List<Task> tasks) {
+        StringBuilder output = new StringBuilder();
         try {
-            add(new Todo(description), tasks);
+            output.append(add(new Todo(description), tasks));
         } catch (EmptyArgumentException e) {
-            System.out.println(e.getMessage());
+            output.append(e.getMessage());
         }
+        return output.toString();
     }
 
-    private static void addDeadline(String argString, List<Task> tasks) {
+    private static String addDeadline(String argString, List<Task> tasks) {
+        StringBuilder output = new StringBuilder();
         try {
             String[] args = argString.split("/by ", 2);
-            add(new Deadline(args[0], args[1]), tasks);
+            output.append(add(new Deadline(args[0], args[1]), tasks));
         } catch (EmptyArgumentException e) {
-            System.out.println(e.getMessage());
+            output.append(e.getMessage());
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("""
-                    Oh quack! I don't know the deadline!
-                    Please specify /by followed by the deadline.""");
+            output
+                    .append("Oh quack! I don't know the deadline!\n")
+                    .append("Please specify /by followed by the deadline.");
         }
+        return output.toString();
     }
 
-    private static void addEvent(String argString, List<Task> tasks) {
+    private static String addEvent(String argString, List<Task> tasks) {
+        StringBuilder output = new StringBuilder();
         try {
             String[] argsFrom = argString.split("/from ", 2);
             String[] argsTo = argsFrom[1].split("/to ", 2);
-            add(new Event(argsFrom[0], argsTo[0], argsTo[1]), tasks);
+            output.append(add(new Event(argsFrom[0], argsTo[0], argsTo[1]), tasks));
         } catch (EmptyArgumentException e) {
-            System.out.println(e.getMessage());
+            output.append(e.getMessage());
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("""
-                    Oh quack! I don't know the start and/or end times!
-                    Please specify /from followed by the start time, followed by /to and the end time.""");
+            output
+                    .append("Oh quack! I don't know the start and/or end times!\n")
+                    .append("Please specify /from followed by the start time, followed by /to and the end time.");
         }
+        return output.toString();
     }
 
-    private static void add(Task task, List<Task> tasks) {
+    private static String add(Task task, List<Task> tasks) {
         tasks.add(task);
-        System.out.printf("""
-                Quack. I've added this task:
-                %s
-                Now you have %d task%s in the list.
-                """, task, tasks.size(), tasks.size() == 1 ? "" : "s");
+        return new StringBuilder()
+                .append("Quack. I've added this task:\n")
+                .append(task)
+                .append("\nNow you have ")
+                .append(tasks.size())
+                .append(" task")
+                .append(tasks.size() == 1 ? "" : "s")
+                .append(" in the list.")
+                .toString();
     }
 
-    private static void list(List<Task> tasks) {
-        System.out.println("Here are your tasks. Let's get quacking!");
+    private static String list(List<Task> tasks) {
+        StringBuilder output = new StringBuilder().append("Here are your tasks. Let's get quacking!");
         for (int i = 0; i < tasks.size(); i++) {
-            System.out.printf("%d. %s%n", i + 1, tasks.get(i));
+            output.append("\n").append(i + 1).append(". ").append(tasks.get(i));
         }
+        return output.toString();
     }
 
     private static void greet() {
