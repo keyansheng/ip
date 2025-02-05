faee16fda55b6a56da3aa2962a46ee26d7f726bb
keyansheng <65121402+keyansheng@users.noreply.github.com>
2025-02-05 19:44:57 +0800

    Break before operator

--- src/main/java/rubberduke/util/TaskList.java
+++ src/main/java/rubberduke/util/TaskList.java
@@ -50,8 +50,8 @@ public class TaskList {
         try {
             Task task = tasks.remove(Integer.parseInt(number.strip()) - 1);
             return "Quack. I've removed this task:\n"
-                   + task + "\n" +
-                   "Now you have " + tasks.size() + " task" + (tasks.size() == 1 ? "" : "s") + " in the list.";
+                    + task + "\n"
+                    + "Now you have " + tasks.size() + " task" + (tasks.size() == 1 ? "" : "s") + " in the list.";
         } catch (NumberFormatException e) {
             return "Oh quack! I can't read this number! Please specify the task number.";
         } catch (IndexOutOfBoundsException e) {
